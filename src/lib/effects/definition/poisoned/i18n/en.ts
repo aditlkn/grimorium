43dd5b17c65fadc4fe5d75ca83f449a6cdd0b2a5
Adnan A M <adnan.tlkn@gmail.com>
2026-04-14 11:23:38 +0530

    Add character-universe certification harness and CI tiers

--- src/lib/effects/definition/poisoned/i18n/en.ts
+++
@@ -1,7 +0,0 @@
-const en = {
-  name: 'Poisoned',
-  description:
-    "This player's ability malfunctions tonight. Information roles get wrong info, passive abilities fail.",
-} as const
-
-export default en
